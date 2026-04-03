e4e983034eb58a757a3bed70587643989aeef0d9
shasidumalshan <shasidumalshan9579@gmail.com>
2026-04-03 11:24:10 +0530

    feat: create Student request and response DTOs

--- src/main/java/com/example/student_service_management_system/dto/StudentRequestDTO.java
+++ src/main/java/com/example/student_service_management_system/dto/StudentRequestDTO.java
@@ -1,4 +1,17 @@
 package com.example.student_service_management_system.dto;
 
+import lombok.Data;
+import jakarta.validation.constraints.*;
+
+@Data
 public class StudentRequestDTO {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String course;
 }
--- src/main/java/com/example/student_service_management_system/dto/StudentResponseDTO.java
+++ src/main/java/com/example/student_service_management_system/dto/StudentResponseDTO.java
@@ -1,4 +1,14 @@
 package com.example.student_service_management_system.dto;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
 public class StudentResponseDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String course;
+    private LocalDateTime createdAt;
 }
